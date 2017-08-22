11eb766247e9b5444929291a4886c54f24a99733
Elbin Keeppanasseril <elbincodes@gmail.com>
2017-08-22 04:28:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   myStr = ''
+#   counter = 0
+#   while counter < array.length-1
+#     myStr = myStr + array[counter] + ", "
+#     counter += 1
+#   end
+#   myStr = myStr + "and " + array.last
+#   myStr
+# end
 
+
+
+def oxford_comma(array)
+ if array.length > 2
+x = array.pop
+array.join(", ") + ", and " + x
+ else
+   array.join(" and ")
+ end
 end
